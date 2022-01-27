22550f69a177efb1a3c4648f086b2517bc239c1a
Sankalp <sankalp.bhatnagar159@gmail.com>
2022-01-27 23:11:21 +0530

    first

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import CButton from './components/Button';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  let incrementCount = () => {
+    setCount(count + 1);
+  };
+  let decrementCount = () => {
+    setCount(count - 1);
+  };
+  let resetCount = () => {
+    setCount(0);
+  };
+  return (
+    <View style={styles.container}>
+    <Text style={styles.counter}>Counter</Text> 
+    <Text style={styles.t2}>{count}</Text>
+    <View style={styles.box2}>
+    <CButton title="+" action={incrementCount} />
+    <CButton title="-" action={decrementCount} />
+    </View>    
+    <CButton title="Reset" action={resetCount} /> 
+    </View>  
+)}
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box2:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'red',
+    width:'50%',
+  },
+  t2:{
+    fontSize:20,
+  },
+  counter: {
+    fontSize: 25,
+  }
+});
